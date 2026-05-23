921c454ac5d3023712e7ea4ff4af6f8c2ae3f2df
MashrurNafi <mashrurnafi2k2@gmail.com>
2026-05-23 18:52:48 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,16 @@ import type { Application } from "express-serve-static-core";
 import { userRoute } from "./modules/user/user.route";
 import { authRoute } from "./modules/auth/auth.route";
 import { issuesRoute } from "./modules/issues/issues.route";
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: 'http://localhost:5000',
+}));
 
 app.get("/", (res: Response) => {
   res.status(200).json({
